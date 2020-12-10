9f6a1af35a20f8ddb9dbba6505ffb298bf3e4719
REIFS HERNANDO <ereifs@serikat.es>
2020-12-10 20:36:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+/* const Pokemon = (props) => { */
+const Pokemon = ({url}) => { 
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        setPokemon(res.pokemon);
+      
+      });
+/*   }, [props.url]); */
+  }, [url]);
+
+  return (
+    <div>
+      <p>{pokemon[Math.floor(Math.random() * pokemon.length)].pokemon.name}</p> 
+      <p>{pokemon[Math.floor(Math.random() * pokemon.length)].pokemon.name}</p>
+      <p>{pokemon[Math.floor(Math.random() * pokemon.length)].pokemon.name}</p>
+    </div>
+  ) 
+ 
+}
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/type/")
+      .then(res => res.json())
+      .then(res => {
+        setData(res.results)
+      });
+  }, []);
+
+
+
+   const mostrarTipo = data.map((tipo,index) => 
+    // <option key={index+1} value={index+1}>{tipo.name}</option>
+    <option key={index} value={tipo.url}>{tipo.name}</option>
+  ) 
+
+    const handleChange = (e) => { 
+    setUrl(e.target.value)
+  }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <select onChange={handleChange}>{mostrarTipo}</select>
+      <Pokemon url={url} />
+    </>
+  ); 
 }
 
 export default App;
